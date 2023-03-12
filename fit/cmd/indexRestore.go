df8f5b90f36288db97bdf6f9432288224856e9ea
kazurego7 <kazurego7@gmail.com>
2023-03-13 00:59:25 +0900

    fit index restore 実装

--- fit/cmd/indexRestore.go
+++ fit/cmd/indexRestore.go
@@ -9,9 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// indexRemoveCmd represents the indexRemove command
-var indexRemoveCmd = &cobra.Command{
-	Use:   "remove",
+// indexRestoreCmd represents the indexRestore command
+var indexRestoreCmd = &cobra.Command{
+	Use:   "restore",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -27,15 +27,15 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	indexCmd.AddCommand(indexRemoveCmd)
+	indexCmd.AddCommand(indexRestoreCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// indexRemoveCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// indexRestoreCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// indexRemoveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// indexRestoreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
