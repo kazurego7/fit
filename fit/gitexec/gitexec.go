e66cd402b797b024216f0e1efb8839cde1d02323
kazurego7 <kazurego7@gmail.com>
2023-03-12 21:32:14 +0900

    gitコマンドの色が出力されるように修正

--- fit/gitexec/gitexec.go
+++ fit/gitexec/gitexec.go
@@ -10,9 +10,10 @@ import (
 func Git(args ...string) {
 	execMessage := fmt.Sprintf("execute command: git %v \n", strings.Join(args, " "))
 	fmt.Fprint(os.Stdout, execMessage)
-	out, err := exec.Command("git", args...).Output()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-	fmt.Fprint(os.Stdout, string(out))
+	cmd := exec.Command("git", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Start()
+	defer cmd.Wait()
 }
