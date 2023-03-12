67629288b03d287e57ca90b3d168aaa381834c97
kazurego7 <kazurego7@gmail.com>
2023-03-12 22:17:45 +0900

    fit index remove 実装

--- fit/cmd/indexRemove.go
+++ fit/cmd/indexRemove.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"kazurego7/fit/fit/gitexec"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +20,9 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("indexRemove called")
+		gitSubCmd := []string{"restore", "--staged"}
+		allArgs := append(gitSubCmd, args...)
+		gitexec.Git(allArgs...)
 	},
 }
 
