e8e469ece4ae518015b4eca7da29e647accfe900
kazurego7 <kazurego7@gmail.com>
2023-03-13 00:28:50 +0900

    fit reflog 実装

--- fit/cmd/reflog.go
+++ fit/cmd/reflog.go
@@ -1,18 +1,17 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"kazurego7/fit/fit/gitexec"
 
 	"github.com/spf13/cobra"
 )
 
-// cmdlogCmd represents the cmdlog command
-var cmdlogCmd = &cobra.Command{
-	Use:   "cmdlog",
+// reflogCmd represents the reflog command
+var reflogCmd = &cobra.Command{
+	Use:   "reflog",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +20,21 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cmdlog called")
+		gitSubCmd := []string{"reflog"}
+		gitexec.Git(gitSubCmd...)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(cmdlogCmd)
+	rootCmd.AddCommand(reflogCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// cmdlogCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// reflogCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// cmdlogCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// reflogCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
