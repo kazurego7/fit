5fda1f288e56dcb2d6d5bc0a04cbe960ced23ed0
kazurego7 <kazurego7@gmail.com>
2023-03-13 00:47:01 +0900

    fit reflog reset 実装

--- fit/cmd/reflogReset.go
+++ fit/cmd/reflogReset.go
@@ -4,13 +4,14 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	"kazurego7/fit/fit/gitexec"
 
 	"github.com/spf13/cobra"
 )
 
-// cmdlogResetCmd represents the cmdlogReset command
-var cmdlogResetCmd = &cobra.Command{
+// reflogResetCmd represents the reflogReset command
+var reflogResetCmd = &cobra.Command{
 	Use:   "reset",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
@@ -19,21 +20,28 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("args error")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cmdlogReset called")
+		gitSubCmd := []string{"reset", args[0]}
+		gitexec.Git(gitSubCmd...)
 	},
 }
 
 func init() {
-	cmdlogCmd.AddCommand(cmdlogResetCmd)
+	reflogCmd.AddCommand(reflogResetCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// cmdlogResetCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// reflogResetCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// cmdlogResetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// reflogResetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
