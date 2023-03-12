696408045126c6e5f1885ff4d436020eeda8a234
kazurego7 <kazurego7@gmail.com>
2023-03-12 22:08:31 +0900

    fit index load 実装

--- fit/cmd/indexLoad.go
+++ fit/cmd/indexLoad.go
@@ -4,14 +4,14 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"kazurego7/fit/fit/gitexec"
 
 	"github.com/spf13/cobra"
 )
 
-// indexRestoreCmd represents the indexRestore command
-var indexRestoreCmd = &cobra.Command{
-	Use:   "restore",
+// indexLoadCmd represents the indexLoad command
+var indexLoadCmd = &cobra.Command{
+	Use:   "load",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,20 +20,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("indexRestore called")
+		gitSubCmd := []string{"restore"}
+		allArgs := append(gitSubCmd, args...)
+		gitexec.Git(allArgs...)
 	},
 }
 
 func init() {
-	indexCmd.AddCommand(indexRestoreCmd)
+	indexCmd.AddCommand(indexLoadCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// indexRestoreCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// indexLoadCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// indexRestoreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// indexLoadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
