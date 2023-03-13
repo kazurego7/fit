11f81f380215b572aa46f3a1698f681cbb78aee4
kazurego7 <kazurego7@gmail.com>
2023-03-13 23:49:09 +0900

    fit remote add 実装

--- fit/cmd/remoteAdd.go
+++ fit/cmd/remoteAdd.go
@@ -4,7 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"kazurego7/fit/fit/gitexec"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +20,8 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("remoteAdd called")
+		gitSubCmd := []string{"remote", "add", args[0], args[1]}
+		gitexec.Git(gitSubCmd...)
 	},
 }
 
