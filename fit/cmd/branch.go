164ae993fde19026b046d8d37d05367e1c8fcddd
kazurego7 <kazurego7@gmail.com>
2023-03-18 13:19:10 +0900

    リモートブランチがあるか、ローカルブランチとのつながりがあるかを分かるように、fit branch list を修正

--- fit/cmd/branch.go
+++ fit/cmd/branch.go
@@ -19,7 +19,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"branch", "--list"}
+		gitSubCmd := []string{"branch", "--list", "--verbose", "--verbose"}
 		gitexec.Git(gitSubCmd...)
 	},
 }
