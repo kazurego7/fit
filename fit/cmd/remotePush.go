f93457ad8ed907610247a1b44395b4fac5b746af
kazurego7 <kazurego7@gmail.com>
2023-03-18 11:44:46 +0900

    branch 表示で upstream の判別をするため、remote push はデフォルトで upstream をセットする

--- fit/cmd/remotePush.go
+++ fit/cmd/remotePush.go
@@ -19,7 +19,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"push", "origin", "HEAD"}
+		gitSubCmd := []string{"push", "--set-upstream", "origin", "HEAD"}
 		gitexec.Git(gitSubCmd...)
 	},
 }
