f14d800038a494bf25d56e83c525852b5b5f2d7f
kazurego7 <kazurego7@gmail.com>
2023-03-19 20:04:01 +0900

    repository init 実装

--- fit/cmd/repository/init.go
+++ fit/cmd/repository/init.go
@@ -1,8 +1,7 @@
 package repository
 
 import (
-	"fmt"
-
+	"github.com/kazurego7/fit/fit/gitexec"
 	"github.com/spf13/cobra"
 )
 
@@ -16,6 +15,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("repositoryInit called")
+		gitSubCmd := []string{"init"}
+		gitexec.Git(gitSubCmd...)
 	},
 }
