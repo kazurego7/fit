e47d15b1f0a422fea2a05543192ea62889369e50
kazurego7 <kazurego7@gmail.com>
2023-03-21 18:56:57 +0900

    remote push でいちいちHEADを指定するのが面倒くさいので、HEADをデフォルトとする

--- fit/cmd/remote/push.go
+++ fit/cmd/remote/push.go
@@ -15,8 +15,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"push", "origin", args[0], "--prune", "--set-upstream"}
+		gitSubCmd := []string{"push", "origin", refspecFlag, "--prune", "--set-upstream"}
 		fitio.PrintGitCommand(gitSubCmd...)
 		fitio.ExecuteGit(gitSubCmd...)
 	},
+}
+
+var refspecFlag string
+
+func init() {
+	PushCmd.PersistentFlags().StringVar(&refspecFlag, "refspec", "HEAD", `refspec`)
 }
