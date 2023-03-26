eadda8bce2042e315f9dc99ad3adfef6b0e6adc1
kazurego7 <kazurego7@gmail.com>
2023-03-26 17:05:19 +0900

    stash store で保存する種類を worktree, index, すべて, から選べるようにする

--- fit/cmd/stash/store.go
+++ fit/cmd/stash/store.go
@@ -1,6 +1,8 @@
 package stash
 
 import (
+	"errors"
+
 	"github.com/kazurego7/fit/fit/fitio"
 	"github.com/spf13/cobra"
 )
@@ -14,9 +16,28 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"stash", "push", "--include-untracked"}
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var gitSubCmd []string
+		switch storeFlags.kind {
+		case "all":
+			gitSubCmd = []string{"stash", "push", "--include-untracked"}
+		case "index":
+			gitSubCmd = []string{"stash", "push", "--staged"}
+		case "worktree":
+			gitSubCmd = []string{"stash", "push", "--include-untracked", "--keep-index"}
+		default:
+			return errors.New("invalid kind of stash files")
+		}
 		fitio.PrintGitCommand(gitSubCmd...)
 		fitio.ExecuteGit(gitSubCmd...)
+		return nil
 	},
+}
+
+var storeFlags struct {
+	kind string
+}
+
+func init() {
+	StoreCmd.Flags().StringVarP(&storeFlags.kind, "kind", "k", "all", `select kind of stash files from "index", "worktree", "all"`)
 }
