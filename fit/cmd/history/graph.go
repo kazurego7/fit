c3925e9cea068e7a72ec8cf6b3dbde0552408ce4
kazurego7 <kazurego7@gmail.com>
2023-03-29 22:28:04 +0900

    history の引数バリデーション追加

--- fit/cmd/history/graph.go
+++ fit/cmd/history/graph.go
@@ -15,6 +15,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		gitSubCmd := []string{
 			"log",
--- fit/cmd/history/merge.go
+++ fit/cmd/history/merge.go
@@ -15,6 +15,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		gitSubCmd := []string{"merge", args[0]}
 		allArgs := append(gitSubCmd, args...)
