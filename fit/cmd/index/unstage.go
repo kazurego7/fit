11299aa297b74b4c0b9c632159d623ab40ed84e7
kazurego7 <kazurego7@gmail.com>
2023-03-29 22:34:44 +0900

    index の引数バリデーション追加

--- fit/cmd/index/unstage.go
+++ fit/cmd/index/unstage.go
@@ -17,6 +17,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		gitSubCmd := append([]string{"restore", "--staged"}, args...)
 		fitio.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
