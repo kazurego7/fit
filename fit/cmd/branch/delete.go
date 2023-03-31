3865cfb01b7b75239a7fd0b43f4675b9e2dd62d6
kazurego7 <kazurego7@gmail.com>
2023-03-31 15:39:58 +0900

    branch delete を誤っても大した影響はないので、yes/no の確認をやめる

--- fit/cmd/branch/delete.go
+++ fit/cmd/branch/delete.go
@@ -17,24 +17,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if !deleteFlag.yes {
-			var confirmMessage = `do you delete "` + args[0] + `" branch ? [yes/no] : `
-			var cancelMessage = "cancel delete branch"
-			isYes := fitio.InputYesOrNo(confirmMessage, cancelMessage)
-			if !isYes {
-				return
-			}
-		}
 		gitSubCmd := []string{"branch", "--delete", "--force", args[0]}
 		fitio.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
 		fitio.GitCommand(global.Flags.Dryrun, gitSubCmd...)
 	},
-}
-
-var deleteFlag struct {
-	yes bool
-}
-
-func init() {
-	DeleteCmd.Flags().BoolVarP(&deleteFlag.yes, "yes", "y", false, "delete a branch without prompt confirmation")
 }
