1997a36327d63a7c23508476b3e175c25a7b2244
kazurego7 <kazurego7@gmail.com>
2023-04-01 01:15:36 +0900

    stash restore で index を保持したまま失敗した場合、保持せずにリトライする

--- fit/cmd/stash/restore.go
+++ fit/cmd/stash/restore.go
@@ -17,8 +17,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"stash", "apply", args[0]}
-		util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
-		util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
+		exitCode := applyKeepIndex(args[0])
+		if exitCode != 0 {
+			apply(args[0])
+		}
 	},
+}
+
+func applyKeepIndex(stashRevision string) int {
+	gitSubCmd := []string{"stash", "apply", "--quiet", "--index", stashRevision}
+	util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
+	return util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
+}
+
+func apply(stashRevision string) int {
+	gitSubCmd := []string{"stash", "apply", "--quiet", stashRevision}
+	util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
+	return util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
 }
