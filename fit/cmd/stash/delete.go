1d8a4d58a633a6aaa5297b147074e8cef43ef223
kazurego7 <kazurego7@gmail.com>
2023-04-02 20:33:51 +0900

    stash delete にすべてのスタッシュを削除するフラグを追加

--- fit/cmd/stash/delete.go
+++ fit/cmd/stash/delete.go
@@ -1,6 +1,8 @@
 package stash
 
 import (
+	"errors"
+
 	"github.com/kazurego7/fit/fit/global"
 	"github.com/kazurego7/fit/fit/util"
 	"github.com/spf13/cobra"
@@ -15,10 +17,27 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"stash", "drop", args[0]}
+	Args: cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var gitSubCmd []string
+		if deleteFlag.all {
+			gitSubCmd = []string{"stash", "clear"}
+		} else {
+			if len(args) == 0 {
+				return errors.New("require 1 argument")
+			}
+			gitSubCmd = []string{"stash", "drop", args[0]}
+		}
 		util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
 		util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
+		return nil
 	},
+}
+
+var deleteFlag struct {
+	all bool
+}
+
+func init() {
+	DeleteCmd.Flags().BoolVarP(&deleteFlag.all, "all", "a", false, "Delete all stash")
 }
