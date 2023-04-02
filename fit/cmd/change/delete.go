61bc0bfcee1bad600bfd2f739db38488a7c4a69d
kazurego7 <kazurego7@gmail.com>
2023-04-02 15:23:56 +0900

    change delete のリファクタリング

--- fit/cmd/change/delete.go
+++ fit/cmd/change/delete.go
@@ -19,79 +19,13 @@ to quickly create a Cobra application.`,
 		switch {
 		case deleteFlag.worktree:
 			confirmBackup()
-			unergedList := searchWorktreeList("U", args[0])
-			for i, _ := range unergedList {
-				unergedList[i] = ":!" + unergedList[i]
-			}
-			restoreList := searchWorktreeList("", append(unergedList, args[0])...)
-			if len(restoreList) != 0 {
-				exitCode := restoreWorktree(restoreList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-
-			addedList := searchWorktreeList("A", args[0])
-			if len(addedList) != 0 {
-				exitCode := removeIndex(addedList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-			clean(args[0])
+			deleteWorktree(args...)
 		case deleteFlag.index:
 			confirmBackup()
-			indexList := searchIndexList("", args[0])
-			worktreeList := searchWorktreeList("", args[0])
-			indexOnlyList := util.Difference(indexList, worktreeList)
-			restoreList := searchIndexList("a", indexOnlyList...)
-			cleanList := searchIndexList("A", indexOnlyList...)
-			if len(indexList) != 0 {
-				exitCode := restoreIndex(indexList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-			if len(restoreList) != 0 {
-				exitCode := restoreWorktree(restoreList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-			if len(cleanList) != 0 {
-				exitCode := clean(cleanList...)
-				if exitCode != 0 {
-					return
-				}
-			}
+			deleteIndex(args...)
 		case deleteFlag.all:
 			confirmBackup()
-			indexList := searchIndexList("", args[0])
-			if len(indexList) != 0 {
-				exitCode := restoreIndex(indexList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-			addedList := searchWorktreeList("A", args[0])
-			if len(addedList) != 0 {
-				exitCode := removeIndex(addedList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-			restoreList := searchWorktreeList("a", args[0])
-			if len(restoreList) != 0 {
-				exitCode := restoreWorktree(restoreList...)
-				if exitCode != 0 {
-					return
-				}
-			}
-
-			exitCode := clean(args[0])
-			if exitCode != 0 {
-				return
-			}
+			deleteAll(args...)
 		}
 	},
 }
@@ -107,4 +41,80 @@ func init() {
 	DeleteCmd.Flags().BoolVarP(&deleteFlag.index, "index", "i", false, "delete changes index to HEAD")
 	DeleteCmd.Flags().BoolVarP(&deleteFlag.all, "all", "a", true, "delete changes index and worktree")
 	DeleteCmd.MarkFlagsMutuallyExclusive("worktree", "index", "all")
+}
+
+func deleteWorktree(args ...string) {
+	unergedList := searchWorktreeList("U", args[0])
+	for i := range unergedList {
+		unergedList[i] = ":!" + unergedList[i]
+	}
+	restoreList := searchWorktreeList("", append(unergedList, args[0])...)
+	if len(restoreList) != 0 {
+		exitCode := restoreWorktree(restoreList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	addedList := searchWorktreeList("A", args[0])
+	if len(addedList) != 0 {
+		exitCode := removeIndex(addedList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	clean(args[0])
+}
+
+func deleteIndex(args ...string) {
+	indexList := searchIndexList("", args[0])
+	worktreeList := searchWorktreeList("", args[0])
+	indexOnlyList := util.Difference(indexList, worktreeList)
+	restoreList := searchIndexList("a", indexOnlyList...)
+	cleanList := searchIndexList("A", indexOnlyList...)
+	if len(indexList) != 0 {
+		exitCode := restoreIndex(indexList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	if len(restoreList) != 0 {
+		exitCode := restoreWorktree(restoreList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	if len(cleanList) != 0 {
+		exitCode := clean(cleanList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+}
+
+func deleteAll(args ...string) {
+	indexList := searchIndexList("", args[0])
+	if len(indexList) != 0 {
+		exitCode := restoreIndex(indexList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	addedList := searchWorktreeList("A", args[0])
+	if len(addedList) != 0 {
+		exitCode := removeIndex(addedList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	restoreList := searchWorktreeList("a", args[0])
+	if len(restoreList) != 0 {
+		exitCode := restoreWorktree(restoreList...)
+		if exitCode != 0 {
+			return
+		}
+	}
+	exitCode := clean(args[0])
+	if exitCode != 0 {
+		return
+	}
 }
