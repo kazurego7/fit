eefc683a002a49d9578468da1079f649cc886032
kazurego7 <kazurego7@gmail.com>
2023-04-02 16:55:44 +0900

    stash snap は store restore で代用可能なため削除

--- fit/cmd/stash/stash.go
+++ fit/cmd/stash/stash.go
@@ -1,6 +1,8 @@
 package stash
 
 import (
+	"github.com/kazurego7/fit/fit/global"
+	"github.com/kazurego7/fit/fit/util"
 	"github.com/spf13/cobra"
 )
 
@@ -20,7 +22,33 @@ func init() {
 	StashCmd.AddCommand(DeleteCmd)
 	StashCmd.AddCommand(ListCmd)
 	StashCmd.AddCommand(RestoreCmd)
-	StashCmd.AddCommand(SnapCmd)
 	StashCmd.AddCommand(ShowCmd)
 	StashCmd.AddCommand(StoreCmd)
+}
+
+func Snap(stashMessage string) int {
+	exitCode := stashPush(stashMessage)
+	if exitCode != 0 {
+		return exitCode
+	}
+	return stashApply()
+}
+
+func stashPush(stashMessage string) int {
+	var gitSubCmd []string
+	if stashMessage == "" {
+		gitSubCmd = []string{"stash", "push", "--include-untracked"}
+	} else {
+		gitSubCmd = []string{"stash", "push", "--include-untracked", "--message", stashMessage}
+	}
+	util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
+	exitCode := util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
+	return exitCode
+}
+
+func stashApply() int {
+	gitSubCmd := []string{"stash", "apply", "--index", "--quiet"}
+	util.PrintGitCommand(global.Flags.Dryrun, gitSubCmd...)
+	exitCode := util.GitCommand(global.Flags.Dryrun, gitSubCmd...)
+	return exitCode
 }
