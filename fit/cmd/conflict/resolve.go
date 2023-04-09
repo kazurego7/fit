d1de968765570a5136f78b46eb58ea79ba2985e0
kazurego7 <kazurego7@gmail.com>
2023-04-09 18:02:51 +0900

    conflict resolve の引数の必須条件が間違っていたのを修正

--- fit/cmd/conflict/resolve.go
+++ fit/cmd/conflict/resolve.go
@@ -9,7 +9,7 @@ import (
 var ResolveCmd = &cobra.Command{
 	Use:   "resolve",
 	Short: "マージコンフリクトを解消し、マージコミットを作成する.",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		gitSubCmd := []string{"merge", "--continue"}
 		util.GitCommand(global.RootFlag, gitSubCmd...)
