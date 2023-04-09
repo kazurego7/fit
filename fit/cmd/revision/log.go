37b2107192cb44776d73ce9e6cf55b19ba407d20
kazurego7 <kazurego7@gmail.com>
2023-04-09 18:04:03 +0900

    revision log でデフォルトはスタッシュを表示せずにフラグで指定するように修正

--- fit/cmd/revision/log.go
+++ fit/cmd/revision/log.go
@@ -13,22 +13,31 @@ var LogCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		gitSubCmd := []string{
 			"log",
-			"--all",
+			"--branches",
+			"--tags",
+			"--remotes",
 			"--graph",
 			"--abbrev-commit",
 			"--decorate=no",
 			"--date=format: %Y-%m-%d %H:%I:%S",
 			"--format=format: %C(03)%>|(26)%h%C(reset)   %C(bold 1)%d%C(reset) %C(bold 0)%s%C(reset) %>|(140)%C(reset)  %C(04)%ad%C(reset)  %C(green)%<(16,trunc)%an%C(reset)",
 		}
-		if allFlag {
+		if logFlag.orphan {
 			gitSubCmd = append(gitSubCmd, "--reflog")
+		}
+		if logFlag.stash {
+			gitSubCmd = append(gitSubCmd, "--all")
 		}
 		util.GitCommand(global.RootFlag, gitSubCmd...)
 	},
 }
 
-var allFlag bool
+var logFlag struct {
+	orphan bool
+	stash  bool
+}
 
 func init() {
-	LogCmd.Flags().BoolVarP(&allFlag, "all", "a", false, "show all commits.")
+	LogCmd.Flags().BoolVarP(&logFlag.orphan, "orphan", "o", false, "ブランチ・タグの付いていない孤独なコミットもログに表示する(スタッシュも表示される).")
+	LogCmd.Flags().BoolVarP(&logFlag.stash, "stash", "s", false, "スタッシュもログに表示する.")
 }
