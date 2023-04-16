2ae9c4c8d059e329da5fbe35ebf46bc35368a245
kazurego7 <kazurego7@gmail.com>
2023-04-16 22:31:49 +0900

    branch create で、指定したコミットにブランチを作成できるようにする

--- fit/cmd/branch/create.go
+++ fit/cmd/branch/create.go
@@ -8,10 +8,18 @@ import (
 
 var CreateCmd = &cobra.Command{
 	Use:   "create <new branch name>",
-	Short: "現在のコミットにブランチを作成し、そのブランチに移動する.",
+	Short: "指定したコミットにブランチを作成し、そのブランチに移動する.",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"switch", "--create", args[0]}
+		gitSubCmd := []string{"checkout", "-b", args[0], createFlag.revision}
 		util.GitCommand(global.RootFlag, gitSubCmd...)
 	},
+}
+
+var createFlag struct {
+	revision string
+}
+
+func init() {
+	CreateCmd.Flags().StringVarP(&createFlag.revision, "revision", "r", "HEAD", "ブランチ作成先のコミット")
 }
