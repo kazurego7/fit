ff80ea93b0a2174b4ec902f7e22201a04adddcb8
kazurego7 <kazurego7@gmail.com>
2023-10-29 23:08:50 +0900

    branch switch で作業中のファイルを一時保存するように変更

--- pkg/controller/branch/switch.go
+++ pkg/controller/branch/switch.go
@@ -1,6 +1,8 @@
 package branch
 
 import (
+	"strings"
+
 	"github.com/kazurego7/fit/pkg/global"
 	"github.com/kazurego7/fit/pkg/infra/git"
 	"github.com/kazurego7/fit/pkg/util"
@@ -10,11 +12,47 @@ import (
 
 var SwitchCmd = &cobra.Command{
 	Use:   "switch <branch>",
-	Short: "指定したブランチに移動し、ワークツリー・インデックスを復元する.",
+	Short: "指定したブランチに移動し、ワークツリー・インデックスを復元する(作業中のファイルは一時保存する).",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"switch", args[0]}
-		util.GitCommand(global.RootFlag, gitSubCmd)
+		const WIP_MESSAGE = "[WIP]"
+
+		if git.ExistsIndexDiff([]string{":/"}) {
+			git.Commit(WIP_MESSAGE + " Index")
+		}
+
+		if git.ExistsUntrackedFiles([]string{":/"}) || git.ExistsWorktreeDiff([]string{":/"}) {
+			gitSubCmd := []string{"add", ":/"}
+			util.GitCommand(global.RootFlag, gitSubCmd)
+			git.Commit(WIP_MESSAGE + " Worktree")
+		}
+
+		{
+			gitSubCmd := []string{"switch", args[0]}
+			util.GitCommand(global.RootFlag, gitSubCmd)
+		}
+
+		var existsWIPIndex = false
+		{
+			gitSubCmd := []string{"log", "--format=%B -n 1", "HEAD"}
+			out, _, _ := util.GitQuery(global.RootFlag, gitSubCmd)
+			existsWIPIndex = strings.HasPrefix(string(out), WIP_MESSAGE)
+		}
+		if existsWIPIndex {
+			gitSubCmd := []string{"reset", "--mixed", "HEAD^"}
+			util.GitCommand(global.RootFlag, gitSubCmd)
+		}
+
+		var existsWIPWorktree = false
+		{
+			gitSubCmd := []string{"log", "--format=%B -n 1", "HEAD"}
+			out, _, _ := util.GitQuery(global.RootFlag, gitSubCmd)
+			existsWIPWorktree = strings.HasPrefix(string(out), WIP_MESSAGE)
+		}
+		if existsWIPWorktree {
+			gitSubCmd := []string{"reset", "--soft", "HEAD^"}
+			util.GitCommand(global.RootFlag, gitSubCmd)
+		}
 	},
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		branchNameList, err := git.GetBranchNameListInUpdateOrder()
