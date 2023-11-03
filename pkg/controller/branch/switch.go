f686f66c50bc2655158f9fdd91a7c9e409736cda
kazurego7 <kazurego7@gmail.com>
2023-11-03 19:26:42 +0900

    feat: branch switchに作業中のファイルを一時保存しないフラグを追加

--- pkg/controller/branch/switch.go
+++ pkg/controller/branch/switch.go
@@ -12,7 +12,12 @@ var SwitchCmd = &cobra.Command{
 	Short: "指定したブランチに移動し、ワークツリー・インデックスを復元する(作業中のファイルは一時保存する).",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		service.SwitchBranchAfterWIP(args[0])
+		branch := args[0]
+		if switchFlag.noWip {
+			git.SwitchBranch(branch)
+		} else {
+			service.SwitchBranchAfterWIP(branch)
+		}
 	},
 	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		branchNameList, err := git.GetBranchNameListInUpdateOrder()
@@ -21,4 +26,12 @@ var SwitchCmd = &cobra.Command{
 		}
 		return branchNameList, cobra.ShellCompDirectiveNoFileComp
 	},
+}
+
+var switchFlag struct {
+	noWip bool
+}
+
+func init() {
+	SwitchCmd.Flags().BoolVar(&switchFlag.noWip, "no-wip", false, "作業中のファイルを一時保存・復元せずにブランチを移動する")
 }
