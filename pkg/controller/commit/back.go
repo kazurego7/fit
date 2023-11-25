2d56a259c9752baca3007a496cd754e2cfff7843
kazurego7 <kazurego7@gmail.com>
2023-11-25 10:30:38 +0900

    commit back の git reset 処理をgitパッケージのものを利用するようにする

--- pkg/controller/commit/back.go
+++ pkg/controller/commit/back.go
@@ -1,9 +1,8 @@
 package commit
 
 import (
-	"github.com/kazurego7/fit/pkg/global"
+	"github.com/kazurego7/fit/pkg/infra/git"
 	"github.com/kazurego7/fit/pkg/service"
-	"github.com/kazurego7/fit/pkg/util"
 
 	"github.com/spf13/cobra"
 )
@@ -13,7 +12,6 @@ var BackCmd = &cobra.Command{
 	Short: "ワークツリー・インデックスを復元せずに、1つ前のコミットに移動する.",
 	Args:  cobra.MatchAll(cobra.NoArgs, service.CurrentIsNotReadonly()),
 	Run: func(cmd *cobra.Command, args []string) {
-		gitSubCmd := []string{"reset", "--soft", "HEAD^"}
-		util.GitCommand(global.RootFlag, gitSubCmd)
+		git.ResetHeadWithoutWorktreeAndIndex()
 	},
 }
