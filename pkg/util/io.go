082d39ce9300d935317942db7433d4cc86115986
kazurego7 <kazurego7@gmail.com>
2024-03-10 17:59:29 +0900

    fix: git の参照系コマンドをキャッシュする

--- pkg/util/io.go
+++ pkg/util/io.go
@@ -9,9 +9,22 @@ import (
 	"github.com/kazurego7/fit/pkg/global"
 )
 
-func GitCommand(globalFlag global.GlobalFlag, args []string) int {
-	extArgs := append([]string{"-c", "core.quotepath=false"}, args...)
+var (
+	queryCache = make(map[string]gitQueryResult)
+)
 
+type gitQueryResult struct {
+	out      []byte
+	exitCode int
+	err      error
+}
+
+func GitCommand(globalFlag global.GlobalFlag, args []string) int {
+	// 更新時にクエリ結果のキャッシュをクリア
+	queryCache = make(map[string]gitQueryResult)
+
+	// デバッグモードの場合は標準エラー出力にコマンドを表示
+	extArgs := append([]string{"-c", "core.quotepath=false"}, args...)
 	if global.RootFlag.Debug {
 		if globalFlag.Dryrun {
 			fmt.Fprintln(os.Stderr, "dry-run: git "+strings.Join(extArgs, " "))
@@ -20,10 +33,12 @@ func GitCommand(globalFlag global.GlobalFlag, args []string) int {
 		}
 	}
 
+	// ドライランの場合は実行せずに終了コード 0 を返す
 	if globalFlag.Dryrun {
 		return 0
 	}
 
+	// 実行して終了コードを返す
 	cmd := exec.Command("git", extArgs...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -33,15 +48,28 @@ func GitCommand(globalFlag global.GlobalFlag, args []string) int {
 }
 
 func GitQuery(globalFlag global.GlobalFlag, args []string) ([]byte, int, error) {
+	// デバッグモードの場合は標準エラー出力にクエリを表示
 	extArgs := append([]string{"-c", "core.quotepath=false"}, args...)
-
+	extArgsString := strings.Join(extArgs, " ")
+	cache, cacheExists := queryCache[extArgsString]
 	if global.RootFlag.Debug {
-		fmt.Fprintln(os.Stderr, "query: git "+strings.Join(extArgs, " "))
+		queryResult := "query(execute): git " + extArgsString
+		if cacheExists {
+			queryResult = "query(cache): git " + extArgsString
+		}
+		fmt.Fprintln(os.Stderr, queryResult)
 	}
 
+	// クエリ結果のキャッシュがあればそれを返す
+	if cacheExists {
+		return cache.out, cache.exitCode, cache.err
+	}
+
+	// クエリ結果のキャッシュがなければクエリを実行してキャッシュに保存
 	cmd := exec.Command("git", extArgs...)
 	out, err := cmd.Output()
 	exitCode := cmd.ProcessState.ExitCode()
+	queryCache[extArgsString] = gitQueryResult{out, exitCode, err}
 	return out, exitCode, err
 }
 
